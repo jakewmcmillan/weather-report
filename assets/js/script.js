50ff84a02ef6fa1d92e861ffdf58548fe73a22ff
Jake McMillan <jakewmcmillan@gmail.com>
2022-07-31 13:06:28 -0400

    Initial Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,32 @@ var cityOutputEl = document.querySelector('#city-output');
 var cityInputEl = document.querySelector('#city');
 var reposContainerEl = document.querySelector('#repos-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+
+    var cityName = cityInputEl.value.trim();
+
+    if (cityName) {
+        getCityRepos(cityName);
+
+        reposContainerEl.textContent = '';
+        cityInputEl.value = '';
+    } else {
+        alert('Please enter a City name');
+    }
+};
+
+var buttonClickHandler = function (event) {
+    var language = event.target.getAttribute('data-language');
+
+    if (language){
+        getFeaturedRepos(language);
+
+        reposContainerEl.textContent = '';
+    }
+};
+
+var getCityRepos = function (city) {
+    
+}
