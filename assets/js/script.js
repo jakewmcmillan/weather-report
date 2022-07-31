7b51f99d6c42ee8c032807e237d1f8e79e3e7bac
Jake McMillan <jakewmcmillan@gmail.com>
2022-07-31 12:20:26 -0400

    Adding initial javascript variables

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var citySearchEl = document.querySelector('#city-search');
+var cityOutputEl = document.querySelector('#city-output');
+var cityInputEl = document.querySelector('#city');
+var reposContainerEl = document.querySelector('#repos-container');
+var repoSearchTerm = document.querySelector('#repo-search-term');
