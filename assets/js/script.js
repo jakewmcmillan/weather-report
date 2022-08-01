3c5b58f75278c46935a330b203eab12b75487b30
Jake McMillan <jakewmcmillan@gmail.com>
2022-08-01 14:56:35 -0400

    Adding initial js skelton

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,15 +32,33 @@ var buttonClickHandler = function (event) {
 var getCityRepos = function (city) {
     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + city + '&lon=' + city + '&appid=83fbcfea79bf43f1175191821f296c01';
 
-    fetch(apiUrl).then(function) (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-                displayRepos(data.items, language);
-            });
-        } else {
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    displayRepos(data.items, language);
+                });
+            } else {
             alert('Error: '+ response.statusText);
         }
-    };
+    })
+    .catch(function (error) {
+        alert('Unable to retrieve weather');
+    });
+};
+
+var getFeaturedRepos = function (language) {
+    var apiUrl = '' + city;
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayRepos(data.items, city);
+            });
+        } else {
+            alert('Error: ' + response.statusText);
+        }
+    });
 };
 
 var displayRepos = function (repos, searchTerm) {
@@ -52,7 +70,22 @@ var displayRepos = function (repos, searchTerm) {
     repoSearchTerm.textContent = searchTerm;
 
     for (var i = 0; i < repos.length; i++) {
-        var repoName = repos.
+        var repoName = repos[i].coord.lon + repos.coord.lat;
+
+        var repoEl = document.createElement('div');
+        repoEl.classList = 'list-item flex-row justify-space-between align-center';
+
+        var titleEl = document.createElement('span');
+        titleEl.textContent = repoName;
+
+        repoEl.appendChild(titleEl);
+
+        var statusEl = document.createElement('span');
+        statusEl.classList = 'flex-row align-center';
+
+        repoEl.appendChild(statusEl);
+
+        repoContainerEl.appendChild(repoEl);
     }
-    
-}
+};
+
